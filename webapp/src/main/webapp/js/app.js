c7405a5ce0873e77030b027f9064bff58ee69f6c
mdepue <matt.depue@powertargetllc.com>
2014-09-10 21:17:08 +0100

    Created basic UI for capturing all relevant data for order submission.  Still need to post to back-end and potentially change catalogue item selection to a modal search with list

--- webapp/src/main/webapp/js/app.js
+++ webapp/src/main/webapp/js/app.js
@@ -1,12 +1,9 @@
 angular.module('processApp', ['ui.bootstrap'])
   .controller('ProcessController', ['$scope', function($scope) {
-    $scope.campaigns = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
+    $scope.order = {};    
  
-    $scope.submitRequest = function() {
-        var lastName = $scope.lastName;
-        alert(lastName);      
+    $scope.submitOrder = function() {
+        alert("Submitting");
     };
 
     $scope.open = function($event) {
@@ -23,5 +20,20 @@ angular.module('processApp', ['ui.bootstrap'])
 
     $scope.initDate = new Date('2016-15-20');  
     $scope.format = 'dd-MMMM-yyyy';
+
+    $scope.campaigns = [
+      {id:'c1', name:'Campaign 1'},
+      {id:'c2', name:'Campaign 2'},
+      {id:'c3', name:'Campaign 3'},
+      {id:'c4', name:'Campaign 4'},
+      {id:'c5', name:'Campaign 5'}
+    ];
+
+    $scope.catalogueItems = [
+      {id:'c1', name:'CI 1'},
+      {id:'c2', name:'CI 2'},
+      {id:'c3', name:'CI 3'},
+      {id:'c4', name:'CI 4'},
+    ];
     
   }]);
