64d561b3f759f8ff3789ca51a70d43a541812171
mdepue <matt.depue@powertargetllc.com>
2014-09-10 17:43:21 +0100

    Set up basic entry page for capturing customer request.  Added missing rebel.xml file for jrebel usage

--- webapp/src/main/webapp/js/app.js
+++ webapp/src/main/webapp/js/app.js
@@ -1,36 +1,27 @@
-/**
- * App JS - Basic Routing Rules 
- */
-
-var routerApp = angular.module('routerApp', ['ui.router']).directive('bsHolder', function() {
-    return {
-        link: function (scope, element, attrs) {
-            Holder.run({images:element.get(0), nocss:true});
-        }
-    };    
-});
-
-routerApp.config(function($stateProvider, $urlRouterProvider) {    
-    $urlRouterProvider.otherwise('/home');
-    
-    $stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/home',
-            templateUrl: 'partials/home.html'
-        })
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-        	url: '/about',
-            templateUrl: 'partials/about.html'     
-        });
-});
-
-angular.module('routerApp')
-  .controller('NavigationCtrl', ['$scope', '$location', function ($scope, $location) {
-    $scope.isCurrentPath = function (path) {
-      return $location.path() == path;
+angular.module('processApp', ['ui.bootstrap'])
+  .controller('ProcessController', ['$scope', function($scope) {
+    $scope.campaigns = [
+      {text:'learn angular', done:true},
+      {text:'build an angular app', done:false}];
+ 
+    $scope.submitRequest = function() {
+        var lastName = $scope.lastName;
+        alert(lastName);      
     };
-}]);
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.initDate = new Date('2016-15-20');  
+    $scope.format = 'dd-MMMM-yyyy';
+    
+  }]);
